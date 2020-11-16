6f07eb74544c6ea697c155c58f4b30459fb80267
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-16 13:46:40 +0100

    Gör mer ändringar

--- 4.1.py
+++ 4.1.py
@@ -1,9 +1,9 @@
 import random #importerar random
 n=0 
-while n!=1:#gör så at koden loppar tills 1 dycker up
+while n!=0 or 10 or -10:#gör så at koden loppar tills en nolla eller 10 eller -10 dycker up
     n=random.randint(-10,10)#skriver ut random tal från -10 till 10
     print(n)#printar talen
-    if n==1 or n==10 or n==-10:#avslutar lopen och börjar nästa del om en etta skrivs
+    if n==0 or n==10 or n==-10:#avslutar lopen och börjar nästa del om en nlla eller 10 eller -10 skrivs
         a=int(input("Give me the biggest number: "))#Frågar efter den största
         b=int(input("Give me the smallest number: "))#Frågar efter det minsta talet
         if b <=0:
