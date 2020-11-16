0e8c921e7af58bc8ff896f6236161e9ffb24f73d
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-16 13:19:35 +0100

    Inte riktigt det dem ville ha

--- 4.1.py
+++ 4.1.py
@@ -4,3 +4,9 @@ while n!=1:
     n=random.randint(1,10)
     print(n)
     if n==1:
+        a=int(input("Give me the smallest number: "))
+        b=int(input("Give me the biggest number: "))
+        c=int(input("Negative number now: "))
+        if c <=0:
+            print("Smallest number", a)
+            print("Biggest number", b)
