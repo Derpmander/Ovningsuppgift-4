08e58854464f22f13fa29b63784382daada3da6f
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-16 13:34:33 +0100

    >Gjort om koden lite samt kommentarer

--- 4.1.py
+++ 4.1.py
@@ -1,12 +1,11 @@
-import random
-n=0
-while n!=1:
-    n=random.randint(1,10)
-    print(n)
-    if n==1:
-        a=int(input("Give me the smallest number: "))
-        b=int(input("Give me the biggest number: "))
-        c=int(input("Negative number now: "))
-        if c <=0:
-            print("Smallest number", a)
-            print("Biggest number", b)
+import random #importerar random
+n=0 
+while n!=1:#gör så at koden loppar tills 1 dycker up
+    n=random.randint(-10,10)#skriver ut random tal från 1-10
+    print(n)#printar talen
+    if n==1:#avslutar lopen och börjar nästa del
+        a=int(input("Give me the biggest number: "))#Frågar efter den största
+        b=int(input("Give me the smallest number: "))#Frågar efter det minsta talet
+        if b <=0:
+            print("Smallest number", a)#skriver ut det minsta talet
+            print("Biggest number", b)#skriver ut det största talet
